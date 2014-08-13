8d0609aef1f5440f2c051d2347768e520d445a18
André Luiz Ferreia <ferreira.andreluiz@gmail.com>
2014-08-13 00:44:36 -0300

    edit

--- src/br/com/testeAngular/PersonResource.java
+++ src/br/com/testeAngular/PersonResource.java
@@ -17,12 +17,12 @@ public class PersonResource {
 	static List<Person> lists;
 
 	public PersonResource() {
-		if (lists==null) {
-			lists= new ArrayList<Person>();
-			lists.add(new Person(1,"Andre", "33"));
-			lists.add(new Person(2,"Renata", "30"));
-			lists.add(new Person(3,"Vanilda", "55"));
-			lists.add(new Person(4,"Mario", "50"));
+		if (lists == null) {
+			lists = new ArrayList<Person>();
+			lists.add(new Person(1, "Andre", "33"));
+			lists.add(new Person(2, "Renata", "30"));
+			lists.add(new Person(3, "Vanilda", "55"));
+			lists.add(new Person(4, "Mario", "50"));
 		}
 	}
 
@@ -31,24 +31,38 @@ public class PersonResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public void create(Person user) {
 		System.out.println(":::::::::::::create" + user.toString());
-		lists.add(new Person(lists.size()+1,user.getNome(), user.getIdade()));
+		lists.add(new Person(lists.size() + 1, user.getNome(), user.getIdade()));
 		System.out.println(lists.size());
 	}
+
 	@POST
 	@Path("alter")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public void alter(Person user) {
-		System.out.println(":::::::::::::alter" + user.toString());
+		for (int i = 0; i < lists.size(); i++) {
+			if (lists.get(i).getId() == user.getId()) {
+				lists.get(i).setIdade(user.getIdade());
+				lists.get(i).setNome(user.getNome());
+				System.out.println(":::::::::::::alter" + user.toString());
+			}
+		}
 		
+
 	}
+
 	@POST
 	@Path("del")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public void del(Person user) {
-		System.out.println(":::::::::::::del" + user.toString());
-		
+		for (int i = 0; i < lists.size(); i++) {
+			if (lists.get(i).getId() == user.getId()) {
+				lists.remove(i);
+				System.out.println(":::::::::::::del" + lists.size());
+			}
+		}
+
 	}
 
 	@GET
